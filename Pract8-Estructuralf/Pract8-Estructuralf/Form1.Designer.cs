00eb0b15b0b97724a20e0383ae60989828a7c56b
romeltr <romel.rodriguez@utp.ac.pa>
2026-08-24 17:08:37 -0500

    Add files via upload

---
+++ Pract8-Estructuralf/Pract8-Estructuralf/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace Pract8_Estructuralf
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtnum1 = new System.Windows.Forms.TextBox();
+            this.txtnum2 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.aaaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip3 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.txtres = new System.Windows.Forms.TextBox();
+            this.gpope = new System.Windows.Forms.GroupBox();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblop = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.contextMenuStrip2.SuspendLayout();
+            this.gpope.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtnum1
+            // 
+            this.txtnum1.Location = new System.Drawing.Point(46, 86);
+            this.txtnum1.Name = "txtnum1";
+            this.txtnum1.Size = new System.Drawing.Size(119, 22);
+            this.txtnum1.TabIndex = 0;
+            this.txtnum1.Text = "4";
+            // 
+            // txtnum2
+            // 
+            this.txtnum2.Location = new System.Drawing.Point(265, 86);
+            this.txtnum2.Name = "txtnum2";
+            this.txtnum2.Size = new System.Drawing.Size(119, 22);
+            this.txtnum2.TabIndex = 1;
+            this.txtnum2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aaaToolStripMenuItem});
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(103, 28);
+            // 
+            // aaaToolStripMenuItem
+            // 
+            this.aaaToolStripMenuItem.Name = "aaaToolStripMenuItem";
+            this.aaaToolStripMenuItem.Size = new System.Drawing.Size(102, 24);
+            this.aaaToolStripMenuItem.Text = "aaa";
+            // 
+            // contextMenuStrip3
+            // 
+            this.contextMenuStrip3.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip3.Name = "contextMenuStrip3";
+            this.contextMenuStrip3.Size = new System.Drawing.Size(61, 4);
+            // 
+            // txtres
+            // 
+            this.txtres.Location = new System.Drawing.Point(434, 86);
+            this.txtres.Name = "txtres";
+            this.txtres.Size = new System.Drawing.Size(217, 22);
+            this.txtres.TabIndex = 5;
+            this.txtres.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // gpope
+            // 
+            this.gpope.BackColor = System.Drawing.Color.White;
+            this.gpope.Controls.Add(this.radioButton4);
+            this.gpope.Controls.Add(this.radioButton3);
+            this.gpope.Controls.Add(this.radioButton2);
+            this.gpope.Controls.Add(this.radioButton1);
+            this.gpope.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.gpope.Location = new System.Drawing.Point(83, 129);
+            this.gpope.Name = "gpope";
+            this.gpope.Size = new System.Drawing.Size(301, 190);
+            this.gpope.TabIndex = 6;
+            this.gpope.TabStop = false;
+            this.gpope.Text = "Operadores";
+            this.gpope.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(26, 115);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(76, 20);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "División";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(26, 89);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(109, 20);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Multiplicación";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(26, 63);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(64, 20);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Resta";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(26, 37);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(63, 20);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Suma";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(402, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 16);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "=";
+            // 
+            // lblop
+            // 
+            this.lblop.AutoSize = true;
+            this.lblop.Location = new System.Drawing.Point(194, 89);
+            this.lblop.Name = "lblop";
+            this.lblop.Size = new System.Drawing.Size(65, 16);
+            this.lblop.TabIndex = 11;
+            this.lblop.Text = "Operador";
+            this.lblop.Click += new System.EventHandler(this.lblop_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(69, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(275, 16);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Ingrese los valores, y selecciona una opción.";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(488, 282);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 37);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Cerrar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(711, 374);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblop);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gpope);
+            this.Controls.Add(this.txtres);
+            this.Controls.Add(this.txtnum2);
+            this.Controls.Add(this.txtnum1);
+            this.Name = "Form1";
+            this.Text = "Calculadora";
+            this.contextMenuStrip2.ResumeLayout(false);
+            this.gpope.ResumeLayout(false);
+            this.gpope.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtnum1;
+        private System.Windows.Forms.TextBox txtnum2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem aaaToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip3;
+        private System.Windows.Forms.TextBox txtres;
+        private System.Windows.Forms.GroupBox gpope;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblop;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Pract8-Estructuralf/Pract8-Estructuralf/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pract8_Estructuralf
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (decimal.TryParse(txtnum1.Text, out decimal num1) && decimal.TryParse(txtnum2.Text, out decimal num2))
+            {
+                lblop.Text = "+";
+                decimal result = num1 + num2;
+                txtres.Text = result.ToString();
+            }
+            else 
+            {
+                MessageBox.Show("Coloque un valor válido", "Ha habido un problema", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (decimal.TryParse(txtnum1.Text, out decimal num1) && decimal.TryParse(txtnum2.Text, out decimal num2))
+            {
+                lblop.Text = "-";
+                decimal result = num1 - num2;
+                txtres.Text = result.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Coloque un valor válido", "Ha habido un problema", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }            
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (decimal.TryParse(txtnum1.Text, out decimal num1) && decimal.TryParse(txtnum2.Text, out decimal num2))
+            {
+                lblop.Text = "x";
+                decimal result = num1 * num2;
+                txtres.Text = result.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Coloque un valor válido", "Ha habido un problema", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            if (decimal.TryParse(txtnum1.Text, out decimal num1) && decimal.TryParse(txtnum2.Text, out decimal num2))
+            {
+                lblop.Text = "/";
+                decimal result = num1 / num2;
+                txtres.Text = result.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Coloque un valor válido", "Ha habido un problema", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+           
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblop_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            DialogResult resultado = MessageBox.Show("¿Desea cerrar la ventana?", "Confirmar cierre", MessageBoxButtons.YesNoCancel,MessageBoxIcon.Warning);
+
+            if (resultado == DialogResult.Yes)
+            {
+                this.Close();
+            }
+            else if (resultado == DialogResult.No)
+            {
+                // No cerrar la ventana
+            }
+            else if (resultado == DialogResult.Cancel)
+            {
+                // Cancelar la operación
+            }
+        }
+    }
+}
