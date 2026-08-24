d07a2d84bf2d18e4a30b8fbf054a7bead697cbbd
romeltr <romel.rodriguez@utp.ac.pa>
2026-08-24 17:06:59 -0500

    Add files via upload

---
+++ Pract1-Controles/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDia = new System.Windows.Forms.Label();
+            this.lblMes = new System.Windows.Forms.Label();
+            this.lblAnio = new System.Windows.Forms.Label();
+            this.txtdia = new System.Windows.Forms.TextBox();
+            this.txtMes = new System.Windows.Forms.TextBox();
+            this.txtAnio = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblfecha = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblDia
+            // 
+            this.lblDia.AutoSize = true;
+            this.lblDia.Location = new System.Drawing.Point(110, 113);
+            this.lblDia.Name = "lblDia";
+            this.lblDia.Size = new System.Drawing.Size(28, 16);
+            this.lblDia.TabIndex = 0;
+            this.lblDia.Text = "Dia";
+            this.lblDia.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblMes
+            // 
+            this.lblMes.AutoSize = true;
+            this.lblMes.Location = new System.Drawing.Point(110, 154);
+            this.lblMes.Name = "lblMes";
+            this.lblMes.Size = new System.Drawing.Size(33, 16);
+            this.lblMes.TabIndex = 1;
+            this.lblMes.Text = "Mes";
+            this.lblMes.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lblAnio
+            // 
+            this.lblAnio.AutoSize = true;
+            this.lblAnio.Location = new System.Drawing.Point(110, 198);
+            this.lblAnio.Name = "lblAnio";
+            this.lblAnio.Size = new System.Drawing.Size(34, 16);
+            this.lblAnio.TabIndex = 2;
+            this.lblAnio.Text = "Anio";
+            // 
+            // txtdia
+            // 
+            this.txtdia.Location = new System.Drawing.Point(177, 107);
+            this.txtdia.Name = "txtdia";
+            this.txtdia.Size = new System.Drawing.Size(157, 22);
+            this.txtdia.TabIndex = 3;
+            // 
+            // txtMes
+            // 
+            this.txtMes.Location = new System.Drawing.Point(175, 154);
+            this.txtMes.Name = "txtMes";
+            this.txtMes.Size = new System.Drawing.Size(158, 22);
+            this.txtMes.TabIndex = 4;
+            // 
+            // txtAnio
+            // 
+            this.txtAnio.Location = new System.Drawing.Point(174, 198);
+            this.txtAnio.Name = "txtAnio";
+            this.txtAnio.Size = new System.Drawing.Size(159, 22);
+            this.txtAnio.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(164, 277);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 27);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Enviar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblfecha
+            // 
+            this.lblfecha.AutoSize = true;
+            this.lblfecha.Location = new System.Drawing.Point(431, 110);
+            this.lblfecha.Name = "lblfecha";
+            this.lblfecha.Size = new System.Drawing.Size(79, 16);
+            this.lblfecha.TabIndex = 7;
+            this.lblfecha.Text = "La fecha es:\r\n";
+            this.lblfecha.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblfecha);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtAnio);
+            this.Controls.Add(this.txtMes);
+            this.Controls.Add(this.txtdia);
+            this.Controls.Add(this.lblAnio);
+            this.Controls.Add(this.lblMes);
+            this.Controls.Add(this.lblDia);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDia;
+        private System.Windows.Forms.Label lblMes;
+        private System.Windows.Forms.Label lblAnio;
+        private System.Windows.Forms.TextBox txtdia;
+        private System.Windows.Forms.TextBox txtMes;
+        private System.Windows.Forms.TextBox txtAnio;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblfecha;
+    }
+}
+
