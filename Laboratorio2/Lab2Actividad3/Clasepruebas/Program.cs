030941ce0c5fefcdf21c0cca0b5933db4d09b044
ROMEL RODRIGUEZ <romel.rodriguez@utp.ac.pa>
2026-09-06 12:09:31 -0500

    Se agregó validaciones de valores en las siguientes areas: - Validación de un valor correcto tipo: String - Validación de un valor correcto tipo: FlotanteCambios de organización de carpetas mayores de laboratorio 1 y 2.

--- Laboratorio2/Lab2Actividad3/Clasepruebas/Program.cs
+++ Laboratorio2/Lab2Actividad3/Clasepruebas/Program.cs
@@ -5,19 +5,38 @@
         static void Main(string[] args)
         {
             LibroCalificacion myLibro1 = new LibroCalificacion("CS101 Programacion en C#", 1);
-            LibroCalificacion myLibro2 = new LibroCalificacion("CS102 Estructuras de Datos", 2 );
+            LibroCalificacion myLibro2 = new LibroCalificacion("CS102 Estructuras de Datos", 2);
             Console.WriteLine("El nombre del curso es: {0}", myLibro1.NombreCurso);
             Console.WriteLine("El nombre del curso es: {0}", myLibro2.NombreCurso);
 
             Console.WriteLine("\nEscriba el nombre del curso: ");
             string elNombreCurso = Console.ReadLine();
+
+            while (int.TryParse(elNombreCurso, out int cursoId) || string.IsNullOrWhiteSpace(elNombreCurso))
+            {
+                Console.WriteLine("Escriba un nombre del curso válido: ");
+                elNombreCurso = Console.ReadLine();
+            }
+            ;
+
+
             Console.WriteLine("\nEscriba la nota del curso: ");
-            float laNota = float.Parse(Console.ReadLine());
+            //float laNota = Console.ReadLine() is float ? float.Parse(Console.ReadLine()) : 0;
+
+            string laNota = Console.ReadLine();
+            while (!float.TryParse(laNota, out float nota))
+            {
+                Console.WriteLine("Escriba una nota válida: ");
+                laNota = Console.ReadLine();
+            }
+
+            float.Parse(laNota);
+
             myLibro1.NombreCurso = elNombreCurso;
-            myLibro1.Nota = laNota;
+            myLibro1.Nota = float.Parse(laNota);
             Console.WriteLine("El nombre del curso es: {0}", myLibro1.NombreCurso);
             Console.WriteLine("La nota del curso es: {0}", myLibro1.Nota);
-
         }
     }
 }
+
